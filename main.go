39b614ebbcd2b1efb9cd0465c4b9a0fcfd061b9c
Branko Čibej <brane@apache.org>
2022-06-05 07:33:02 +0200

    Update s.Union() -> s.Extend() rename.

--- main.go
+++ main.go
@@ -36,13 +36,13 @@ func main() {
 	print("\nUnion: s1 ∪ s2\n")
 	pset(set.Union(s1, s2))
 	s = s1.Copy()
-	s.Union(s2)
+	s.Extend(s2)
 	pset(s)
 
 	print("\nUnion: s2 ∪ s1\n")
 	pset(set.Union(s2, s1))
 	s = s2.Copy()
-	s.Union(s1)
+	s.Extend(s1)
 	pset(s)
 
 	print("\nIntersection s1 ∩ s2:\n")
